39b1b6e97a3462d8dbd43de72e46bcd477e2abbd
Yanxin Zhou <yzho0062@uni.sydney.edu.au>
2024-10-11 14:06:38 +1100

    Delete payroll directory

--- payroll/src/main/java/com/comp5348/payroll/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.comp5348.payroll.repository;
-
-import com.comp5348.payroll.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
