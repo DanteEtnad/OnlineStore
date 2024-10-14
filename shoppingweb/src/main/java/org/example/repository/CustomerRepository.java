2a1b13607d11c800bb782ba651a99deda7fc4271
Yanxin Zhou <yzho0062@uni.sydney.edu.au>
2024-10-14 19:48:20 +1100

    Delete shoppingweb directory

--- shoppingweb/src/main/java/org/example/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
