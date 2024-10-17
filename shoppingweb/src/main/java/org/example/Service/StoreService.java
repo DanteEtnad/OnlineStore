80a48a0ed61252ad4dd70cbd3c6430d22bac0d32
Yanxin Zhou <yzho0062@uni.sydney.edu.au>
2024-10-18 02:28:58 +1100

    Add files via upload

--- shoppingweb/src/main/java/org/example/Service/StoreService.java
+++ shoppingweb/src/main/java/org/example/Service/StoreService.java
@@ -1,5 +1,9 @@
 package org.example.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StoreService {
 
 }
