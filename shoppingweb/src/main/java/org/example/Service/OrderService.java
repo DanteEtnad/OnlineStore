bfe3a0b83f1a9557eac49e2d46170070f7fc5068
Yanxin Zhou <yzho0062@uni.sydney.edu.au>
2024-10-25 02:51:17 +1100

    store change

--- shoppingweb/src/main/java/org/example/Service/OrderService.java
+++ shoppingweb/src/main/java/org/example/Service/OrderService.java
@@ -5,8 +5,11 @@ import org.example.model.Order;
 import org.example.repository.BankTransferRepository;
 import org.example.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -17,5 +20,23 @@ public class OrderService {
     @Autowired
     private BankTransferRepository bankTransferRepository;
 
+    @Transactional
+    @Scheduled(fixedRate = 5000)
+    public void checkBankTransferStatus() {
+        List<Order> orders = orderRepository.findByStatusIn(List.of("paying"));
 
+        for (Order order : orders) {
+            BankTransfer bankTransfer = order.getBankTransfer();
+
+            if (bankTransfer != null && bankTransfer.getStatus().equals("failed")) {
+                order.setStatus("pending");
+                orderRepository.save(order);
+                System.out.println("Order " + order.getOrderId() + " status set to pending due to failed bank transfer.");
+            }else if (bankTransfer == null) {
+                order.setStatus("pending");
+                orderRepository.save(order);
+                System.out.println("Order " + order.getOrderId() + " has no associated BankTransfer.");
+            }
+        }
+    }
 }
